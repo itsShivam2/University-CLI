716c0730d42fae046c8fbff0b0c5035c6f4a981e
itsShivam2 <shivamgupta7376@gmail.com>
2025-08-10 15:31:11 +0530

    University CLI v1.3 Used volume to persist data.

--- Readme.md
+++ Readme.md
@@ -5,3 +5,13 @@ print("University Management System!")
 
 v1.1
 print("Welcome to the University Management System!")
+
+v1.2
+Code changes to save data to /student_data/students.json file
+
+Tried to use volumes to persist data
+Error - Gave wrong path to save file, data couldn't persist
+
+v1.3
+Remove the error encountered in the v1.2
+Data persisted across containers using volumes
--- student_manager.py
+++ student_manager.py
@@ -1,9 +1,28 @@
-class StudentManager:
-    def __init__(self):
-        self.students = []
+import json
+import os 
 
+
+class StudentManager:
+    def __init__(self, data_file="/app/data/students.json"):
+        self.data_file = data_file
+        self.students = []
+        self.load_students()
+    
+    def load_students(self):
+        if os.path.exists(self.data_file):
+            with open(self.data_file, "r") as f:
+                self.students = json.load(f)
+        else:
+            self.students = [] 
+
+    def save_students(self):
+        os.makedirs(os.path.dirname(self.data_file), exist_ok=True)
+        with open(self.data_file, "w") as f:
+            json.dump(self.students, f, indent=4)   
+        
     def add_student(self, name, age):
         self.students.append({"name": name, "age": age})
+        self.save_students()
         print(f"Student '{name}' added successfully!")
 
     def list_students(self):
